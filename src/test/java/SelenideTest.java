f8bf5cd3d12071ef3c906bd23b3149e9a7f11781
Runciter <unformatmail@gmail.com>
2025-10-06 21:17:10 +0300

    fixes

---
+++ src/test/java/SelenideTest.java
@@ -0,0 +1,38 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class SelenideTest {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+
+    @Test
+    public void testIssueSearch() {
+
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+
+        $(".search-input-container").click();
+        $("#query-builder-test").sendKeys("Runciterr/AllureHW");
+        $("#query-builder-test").submit();
+
+        $(linkText("Runciterr/AllureHW")).click();
+        $("#issues-tab").click();
+        $(withText("#1")).should(Condition.exist);
+    }
+}

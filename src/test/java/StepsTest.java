ff1da446aa141b7bf763b096f9da660ed05f202e
Runciter <unformatmail@gmail.com>
2025-10-06 21:44:17 +0300

    fixes

---
+++ src/test/java/StepsTest.java
@@ -0,0 +1,63 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Allure;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.OutputType;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+public class StepsTest extends TestBase {
+    private static final String REPOSITORY = "Runciterr/AllureHW";
+    private static final int ISSUE = 1;
+
+    @Test
+    public void testLambdaStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открытие главной страницы", () -> {
+            open("https://github.com");
+        });
+        step("Поиск репозитория " + REPOSITORY, () -> {
+            $(".search-input-container").click();
+            $("#query-builder-test").sendKeys(REPOSITORY);
+            $("#query-builder-test").submit();
+        });
+        step("Клик по ссылке репозитория " + REPOSITORY, () -> {
+            $(linkText(REPOSITORY)).click();
+        });
+        step("Клик по Issues", () -> {
+            $("#issues-tab").click();
+        });
+        step("Проверка наличия Issue с номером " + ISSUE, () -> {
+            $(withText("#" + ISSUE)).should(Condition.exist);
+        });
+
+        step("Скриншот", () -> {
+            byte[] screenshot = Selenide.screenshot(OutputType.BYTES);
+            Allure.getLifecycle().addAttachment(
+                    "screenshot",
+                    "image/png",
+                    "png",
+                    screenshot
+            );
+        });
+    }
+
+    @Test
+    public void testAnnotatedStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.clickOnRepositoryLink(REPOSITORY);
+        steps.openIssuesTab();
+        steps.shouldSeeIssueWithNumber(ISSUE);
+        steps.takeScreenshot();
+
+    }
+}
---
+++ src/test/java/WebSteps.java
@@ -0,0 +1,46 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class WebSteps {
+
+    @Step("Открытие главной страницы")
+    public void openMainPage() {
+        open("https://github.com");
+    }
+
+    @Step("Поиск репозитория {repo}")
+    public void searchForRepository(String repo) {
+        $(".search-input-container").click();
+        $("#query-builder-test").sendKeys(repo);
+        $("#query-builder-test").submit();
+    }
+
+    @Step("Клик по ссылке репозитория {repo}")
+    public void clickOnRepositoryLink(String repo) {
+        $(linkText(repo)).click();
+    }
+
+    @Step("Клик по Issues")
+    public void openIssuesTab() {
+        $("#issues-tab").click();
+    }
+
+    @Step("Проверка наличия Issue с номером {issue}")
+    public void shouldSeeIssueWithNumber(int issue) {
+        $(withText("#" + issue)).should(Condition.exist);
+    }
+
+    @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
+    public byte[] takeScreenshot() {
+        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    }
+}
